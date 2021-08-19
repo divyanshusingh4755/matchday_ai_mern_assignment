fa9ada03e9001dd7c19345823725193c3831e749
Divyanshu <divyanshusingh4755@gmail.com>
2021-08-19 09:37:32 +0530

    Some social icon from font awesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
-import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons"
 
 function Footer() {
     return (
@@ -11,11 +12,11 @@ function Footer() {
             <div className="footer-sub-content">
                 <span className="footer-copyright">Copyright | FAQs | Privacy Policy</span>
                 <ul className="footer-social-links">
-                    <li><FontAwesomeIcon icon={faEnvelopeSquare} className="social-icon" /></li>
-                    <li><FontAwesomeIcon icon={faEnvelopeSquare} className="social-icon" /></li>
-                    <li><FontAwesomeIcon icon={faEnvelopeSquare} className="social-icon" /></li>
-                    <li><FontAwesomeIcon icon={faEnvelopeSquare} className="social-icon" /></li>
-                    <li><FontAwesomeIcon icon={faEnvelopeSquare} className="social-icon" /></li>
+                    <li><FontAwesomeIcon icon={faEnvelope} className="social-icon" /></li>
+                    <li><FontAwesomeIcon icon={faInstagram} className="social-icon" /></li>
+                    <li><FontAwesomeIcon icon={faTwitter} className="social-icon" /></li>
+                    <li><FontAwesomeIcon icon={faFacebookF} className="social-icon" /></li>
+                    <li><FontAwesomeIcon icon={faLinkedin} className="social-icon" /></li>
                 </ul>
             </div>
         </div>
